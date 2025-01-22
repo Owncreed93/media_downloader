5e4ac3e253e4312cc4d58eb2f71caecea84f5afa
Christian Tarifeno <christiantarifer@gmail.com>
2025-01-21 20:22:51 -0500

    Updated Download controllers endpoint to send json response

--- api/controllers/download.py
+++ api/controllers/download.py
@@ -1,12 +1,14 @@
 from datetime import datetime
+
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from schemas.download import DownloadCreate, DownloadRequest, DownloadResponse
 from youtbe_downloader.downloader import MediaDownloader
-from utils.main import parse_date, LOCAL_TZ
+from utils.main import LOCAL_TZ
 from api.query.download import save_download
 
-# def create_download(url: str, media_type: str, created_at: str) -> DownloadResponse:
 def create_download(download_data: DownloadRequest, db: Session) -> DownloadResponse:
     '''
     Creates a new download record
@@ -15,30 +17,60 @@ def create_download(download_data: DownloadRequest, db: Session) -> DownloadResp
 
     downloader = MediaDownloader(download_data.url, media_type)
 
-    if downloader.download():
-        download = DownloadCreate(
-            url=download_data.url,
-            youtube_id = downloader.metadata.get('youtube_id'),
-            title=downloader.metadata.get('title'),
-            fulltitle=downloader.metadata.get('fulltitle'),
-            uploader=downloader.metadata.get('uploader'),
-            channel=downloader.metadata.get('channel'),
-            duration=downloader.metadata.get('duration'),
-            label=downloader.metadata.get('label'),
-            copyright=downloader.metadata.get('copyright'),
-            origin_url=downloader.metadata.get('origin_url'),
-            live_status=downloader.metadata.get('live_status'),
-            extractor=downloader.metadata.get('extractor'),
-            categories=downloader.metadata.get('categories'),
-            result=True,
-            media_type=media_type,
-            created_at=datetime.now(LOCAL_TZ)
-        )
+    try:
+        if downloader.download():
+            download = DownloadCreate(
+                url=download_data.url,
+                youtube_id = downloader.metadata.get('youtube_id'),
+                title=downloader.metadata.get('title'),
+                fulltitle=downloader.metadata.get('fulltitle'),
+                uploader=downloader.metadata.get('uploader'),
+                channel=downloader.metadata.get('channel'),
+                duration=downloader.metadata.get('duration'),
+                label=downloader.metadata.get('label'),
+                copyright=downloader.metadata.get('copyright'),
+                origin_url=downloader.metadata.get('origin_url'),
+                live_status=downloader.metadata.get('live_status'),
+                extractor=downloader.metadata.get('extractor'),
+                categories=downloader.metadata.get('categories'),
+                result=True,
+                media_type=media_type,
+                created_at=datetime.now(LOCAL_TZ)
+            )
 
-        new_download = save_download(download, db)
-        response = DownloadResponse.from_sqlalchemy(new_download, download.media_type)
-        print(f'\n*****************\n {downloader.metadata} \n*****************\n')
-        return response
-    else:
-        raise ValueError("Failed to download media.")
-    
+            new_download = save_download(download, db)
+            download_obj = (DownloadResponse.from_sqlalchemy(new_download, download.media_type)).model_dump(mode='json')
+            print(f'\n*****************\n {downloader.metadata} \n*****************\n')
+            
+            return JSONResponse(
+                content={
+                    'status': 'ok',
+                    'download': download_obj,
+                },
+                status_code=200
+            )
+        else:
+            return JSONResponse(
+                content={
+                    'status': 'Error',
+                    'message': 'Failed to download media.'
+                },
+                status_code=400
+            )
+    except ValueError as ve:
+        return JSONResponse(
+            content={
+                'status': 'Error',
+                'message': str(ve),
+            },
+            status_code=400,
+        )
+    except Exception as e:
+        print(str(e))
+        return JSONResponse(
+            content={
+                'status': 'Error',
+                'message': 'Contact system administrator.'
+            },
+            status_code=500
+        )
